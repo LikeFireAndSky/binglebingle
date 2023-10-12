61597c65e5245929d2879c8ae6972545af5058e3
MinSeob <icdmiracle927@gmail.com>
2023-10-12 21:19:26 +0900

    fix

--- components/Home/EnterButton/EnterButton.tsx
+++ components/Home/EnterButton/EnterButton.tsx
@@ -2,11 +2,9 @@
 
 import React, { MouseEventHandler } from 'react';
 import { signIn } from 'next-auth/react';
-import { useRouter } from 'next/navigation';
 import { Button } from '@material-tailwind/react';
 
 const EnterButton = () => {
-	const router = useRouter();
 	const handleClick: MouseEventHandler<HTMLButtonElement> = (e): void => {
 		e.preventDefault();
 		signIn();
