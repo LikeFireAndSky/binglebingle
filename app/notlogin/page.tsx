a381f352ee668fb99f60950e8af52eda49a63133
MinSeob <icdmiracle927@gmail.com>
2023-10-12 23:14:16 +0900

    Feat : 로그인 임포트까지 삭제

--- app/notlogin/page.tsx
+++ app/notlogin/page.tsx
@@ -3,7 +3,6 @@
 import React from 'react';
 import { useRouter } from 'next/navigation';
 import { Button } from '@material-tailwind/react';
-import { signIn, signOut } from 'next-auth/react';
 
 const NOTLOGIN = () => {
 	const router = useRouter();
@@ -11,8 +10,8 @@ const NOTLOGIN = () => {
 	return (
 		<div>
 			NOT LOGIN
-			<Button onClick={() => signIn()}>로그인</Button>
-			<Button onClick={() => signOut()}>로그아웃</Button>
+			<Button>로그인</Button>
+			<Button>로그아웃</Button>
 			<Button onClick={() => router.push('/')}>홈으로</Button>
 		</div>
 	);
