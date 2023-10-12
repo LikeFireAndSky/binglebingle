4e8ddfe5b0b30ea5fe55f000a490f1fdb750e2f1
MinSeob <icdmiracle927@gmail.com>
2023-10-12 23:21:08 +0900

    Feat : mui 오류 확인

--- app/page/page.tsx
+++ app/page/page.tsx
@@ -1,18 +1,17 @@
 'use client';
 
 import React, { MouseEventHandler } from 'react';
-import { Card, Button } from '@material-tailwind/react';
 
 const Page = () => {
 	return (
 		<div className="w-4/5 h-screen flex flex-col mx-auto my-auto items-center justify-center">
-			<Card className="text-5xl flex flex-col justify-center items-center">
+			<div className="text-5xl flex flex-col justify-center items-center">
 				<h1>
 					<strong>돌려돌려 돌림판</strong> 시작해볼까요?
 				</h1>
 				<div className="w-4/5 h-96 bg-primary-color"></div>
-				<Button type="button">로그아웃</Button>
-			</Card>
+				<button type="button">로그아웃</button>
+			</div>
 		</div>
 	);
 };
