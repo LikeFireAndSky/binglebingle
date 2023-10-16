268db9d0c9e181f073c314722199bfb048f210bd
jongsujin <jjs9836@naver.com>
2023-10-16 18:53:35 +0900

    fix: 배포 오류 해결

--- app/calander/page.tsx
+++ app/calander/page.tsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from 'react';
 
 const Calender = () => {
-  return (
-    <div>Calender</div>
-  )
-}
+	return <div>Calender</div>;
+};
 
 export default Calender;
