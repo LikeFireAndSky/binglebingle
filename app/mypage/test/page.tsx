60d5cf613bf9931f21540faab5b279400ae9a335
MinSeob <icdmiracle927@gmail.com>
2023-10-22 19:16:21 +0900

    Feat : queryString 제외하고 바로 user 호출 가능한지 확인

---
+++ app/mypage/test/page.tsx
@@ -0,0 +1,34 @@
+'use client';
+
+import React from 'react';
+import axios from 'axios';
+import { useSession } from 'next-auth/react';
+import { useQuery } from '@tanstack/react-query';
+
+const getAllUsers = async (userUid: string) => {
+	const data = await axios.get(`/api/testuser/${userUid}`);
+	if (!data) {
+		throw new Error('Network response was not ok');
+	}
+	return data;
+};
+
+const Testpage = () => {
+	const { data: session } = useSession({
+		required: true,
+		onUnauthenticated() {
+			setTimeout(() => {
+				alert('로그인이 필요한 서비스입니다.');
+			});
+		},
+	});
+
+	const { data, error, isLoading } = useQuery(['testuser'], () =>
+		getAllUsers(session?.user.uid as string),
+	);
+
+	console.log(data);
+	return <div>{data && data.data[0].name}</div>;
+};
+
+export default Testpage;
